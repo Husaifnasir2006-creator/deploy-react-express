dbcd1bbcf592bc80c2182dc61ca550beb4908a69
Muhammad Husaif Bin Nasir <husaifnasir2006@gmail.com>
2026-09-17 21:25:20 +0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Express server is running",
+        environment: process.env.NODE_ENV || "development"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
